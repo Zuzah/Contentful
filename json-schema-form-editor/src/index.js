7174de221e4c0b63a5921cf806c5dc7e0727d619
Zuzah <murtaza.hasni@gmail.com>
2021-02-24 11:08:45 -0500

    feat: added new extension to translate json schema structure into form representation

--- json-schema-form-editor/src/index.js
+++ json-schema-form-editor/src/index.js
@@ -1,56 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { TextInput } from '@contentful/forma-36-react-components';
 import { init } from 'contentful-ui-extensions-sdk';
-import '@contentful/forma-36-react-components/dist/styles.css';
+import JsonEditor from './components/JsonEditor.js';
+
 import './index.css';
 
-export const App = ({sdk}) => {
-  const [value, setValue] = useState(sdk.field.getValue() || '');
-
-  const onExternalChange = value => {
-    setValue(value);
-  }
-
-  const onChange = e => {
-    const value = e.currentTarget.value;
-    setValue(value);
-    if (value) {
-      sdk.field.setValue(value);
-    } else {
-      sdk.field.removeValue();
-    }
-  }
-
-  useEffect(() => {
-    sdk.window.startAutoResizer();
-  }, []);
-
-  useEffect(() => {
-    // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
-    const detatchValueChangeHandler = sdk.field.onValueChanged(onExternalChange);
-    return detatchValueChangeHandler;
-  });
-
-  return (
-    <TextInput
-      width="large"
-      type="text"
-      id="my-field"
-      testId="my-field"
-      value={value}
-      onChange={onChange}
-    />
-  );
-}
-
-App.propTypes = {
-  sdk: PropTypes.object.isRequired
-};
-
 init(sdk => {
-  ReactDOM.render(<App sdk={sdk} />, document.getElementById('root'));
+  ReactDOM.render(<JsonEditor sdk={sdk} />, document.getElementById('root'));
 });
 
 /**
